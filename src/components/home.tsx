2f8baea091a7ad349c2d2d9bfbb8e039f0c9d3d6
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2024-04-29 00:02:14 +0200

    animation changes

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import gsap, { random } from "gsap";
+import { gsap, random } from "gsap";
 
 export default function Home() {
 
@@ -17,12 +17,12 @@ export default function Home() {
 
         tl.fromTo(".name-animation", 
         {
-          x: -100, 
+          y: -100, 
           opacity: 0, 
-          rotate: -10
+          rotate: -20
         }, 
         {
-          x: 0, 
+          y: 0, 
           opacity: 1, 
           rotate: 0, 
           ease: "expo.out",
@@ -64,14 +64,18 @@ export default function Home() {
     }
 
     return (
-      <div ref={component} className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
-        <div className="col-start-1 md:row-start-1">
-          <h1 className="mb-8 text-[clamp(3rem,13vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={firstName + " " + lastName}>
-            <span className="block text-gray-300">{renderLetters(firstName, "first")}</span>
-            <span className="-mt-[.2em] block text-gray-500">{renderLetters(lastName, "last")}</span>
-          </h1>
-          <span className="job-title block bg-gradient-to-tr from-green-500 via-blue-400 to-green-300 bg-clip-text text-2xl font-bold uppercase tracking[.2em] text-transparent opacity-0 md:text-4xl">{title}</span>
+      <section className="px-4 py-10 md:px-6 md:py-14 lg:py-16">
+        <div className="mx-auto w-full max-w-7xl">
+          <div ref={component} className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+            <div className="col-start-1 md:row-start-1">
+              <h1 className="mb-8 text-[clamp(3rem,12vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={firstName + " " + lastName}>
+                <span className="block text-gray-300">{renderLetters(firstName, "first")}</span>
+                <span className="-mt-[.2em] block text-gray-500">{renderLetters(lastName, "last")}</span>
+              </h1>
+              <span className="job-title block bg-gradient-to-tr from-green-500 via-blue-400 to-green-300 bg-clip-text text-2xl font-bold uppercase tracking[.2em] text-transparent opacity-0 md:text-4xl">{title}</span>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     );
   }
