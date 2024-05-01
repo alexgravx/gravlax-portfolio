1e4d5ba3c51b7b4cd44284c2412855b2ede9e1e2
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2024-05-01 14:44:43 +0200

    techlist animation complete

--- src/components/tech-list.tsx
+++ src/components/tech-list.tsx
@@ -1,6 +1,11 @@
-import { gsap, random } from "gsap";
-import React from "react"
+"use client";
+
+import React, { useEffect, useRef } from "react"
 import { MdCircle } from "react-icons/md"
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger)
 
 export default function TechList() {
 
@@ -10,17 +15,56 @@ export default function TechList() {
       }
 
     const items:techitem[] = [
-        {tech_name:"GSAP", tech_color: "blue"},
-        {tech_name:"React", tech_color: "red"}
+        {tech_name:"Frontend", tech_color: "#ed8c36"},
+        {tech_name:"Backend", tech_color: "#fefce4"},
+        {tech_name:"Mobile", tech_color: "#f5c7f8"},
+        {tech_name:"DevOps", tech_color: "#88e57b"},
         ]
 
+    const component = useRef(null)
+
+    useEffect(() => {
+        let ctx = gsap.context(() => {
+            
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: component.current,
+                    start: "top bottom",
+                    end: "bottom top",
+                    scrub: 4,
+                }
+            });
+
+            tl.fromTo(
+                ".tech-row",
+                {
+                    x: (index) => {
+                        return index % 2 === 0 
+                        ? gsap.utils.random(400, 200) 
+                        : gsap.utils.random(-400, -200);
+                    } 
+                },
+                {
+                    x: (index) => {
+                        return index % 2 === 0 
+                        ? gsap.utils.random(-400, -200) 
+                        : gsap.utils.random(400, 200);
+                    }, 
+                    ease: "power1.inOut",
+                }
+            )
+        }, component)
+        return () => ctx.revert() //cleanup
+    })
+
     return (
-        <section>
-            <h1>
+        <>
+        <section className="overflow-hidden" ref={component}>
+            <h1 className="px-4 py-10 md:px-6 md:py-14 lg:py-16">
             Mes Compétences
             </h1>
             {items.map(({tech_name, tech_color}, index) => (
-                <div key={index} className="tech-row mb-4 flex items-center justify-center gap-4 text-slate-700">
+                <div key={index} className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700" aria-label={tech_name}>
                     {Array.from({length: 15}, (_, index) => (
                         <React.Fragment key={index}>
                             <span className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
@@ -37,6 +81,8 @@ export default function TechList() {
                 </div>
             ))}
         </section>
+        <div className="h-[500vh]"></div>
+        </>
 
     );
   }
