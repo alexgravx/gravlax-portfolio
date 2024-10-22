9ee2b4fb9069233612b0a17c51de3f8222f70419
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2024-10-22 22:52:34 +0200

    [ADD] Speed insights Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 
-import Header from "@/components/Header"
 import Footer from "@/components/Footer";
-import CustomCursor from "@/components/CustomCursor";
 import clsx from "clsx";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
@@ -22,10 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-slate-900 text-slate-100">
       <body className={clsx(urbanist.className, "relative min-h-screen")}>
-        {/* <Header /> */}
         {children}
         <Footer />
-        {/* <div className="absolute inset-0 -z-50 max-h-screen background-gradient"></div> */}
+        <SpeedInsights />
         <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
       </body>
     </html>
