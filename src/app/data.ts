07c33392cc38b923c576fec51794050db34aa686
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2024-12-02 23:58:15 +0100

    [UPDATE] update data

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,22 +1,25 @@
-const bio = "I am a MEng student at CentraleSupelec, 2nd highly selective Engineering french school and 16th Engineering school worldwide."
-+ " I am curious, hard-working, and particularly motivated by the subjects I am passionate about: computer science, chemistry, physics."
+const bio = "I am a Master student at CentraleSupelec, 2nd highly selective Engineering french school and 16th Engineering school worldwide."
++ " I am curious, hard-working, and particularly motivated by the subjects I am passionate about: computer science, maths, physics."
 + " My skills of analysis and synthesis allow me to lead projects with efficiency. My thoroughness and my ability to listen to advice are part of my strengths."
 + " In addition to my engineering studies, I am a member of several associations that allow me to develop my technical, artistic and soft skills."
 
 const education = [
-  {title: "CentraleSupélec", time_period: "2022-2026", institution: "Master Engineering degree", description: "Coursework:"
+  {title: "CentraleSupélec", time_period: "2022-2026", institution: "Master of Engineering", description: "Coursework:"
   +"Computer Science: Web Data Intelligence, Information Systems, Algorithms, Networks and Security;"
   +"Corporate: Corporate finance, Business management, Law (contracts and intellectual property), Economy & Industrial Engineering;"
   +"Physics: Fluid mechanics, Study of materials, Satellite imagery analysis with radiometry and polarimetry;"
   +"Mathematics: Integration, Probability, Partial Differential Equations;"},
-  {title: "Collège Stanislas Paris", time_period: "2013-2022", institution: "Scientific bachelor degree", description: "I followed an intensive two-year preparation in Mathematics, Physics, Engineering, Computer Science & Chemistry for the highly selective French School of Engineering (2020-2022)"},
+  {title: "Collège Stanislas Paris", time_period: "2020-2022", institution: "Preparation courses", 
+    description: "I followed an intensive two-year preparation in Mathematics, Computer Science, Physics,"
+    + "Engineering & Chemistry for the highly selective French School of Engineering. This is equivalent to a bachelor in engineering,"
+    + "with a major in math and computer science"},
 ] 
 
 const experiences = [
-  {title: "Software Engineer", time_period: "2024", institution: "Paris Digital Lab", description: "Prototyping tech projects for major corporations and start-ups through MVPs."},
+  {title: "Data Engineer", time_period: "Fall 2024", institution: "Forvis Mazars", description: "Data Engineering in Data Services team of Forvis Mazars. Working on applied and R&D projects."},
+  {title: "Software Engineer", time_period: "Spring 2024", institution: "Paris Digital Lab", description: "Prototyping tech projects for major corporations and start-ups through MVPs."},
   {title: "DevOps Engineer", time_period: "2023-2024", institution: "ViaRézo", description: "ViaRézo provides internet access to 2000+ residents on Paris-Saclay campus and operates several websites and applications related to CentraleSupélec's associative life through an OpenStack virtualization cluster. The whole infrastructure is monitored and protected by backup systems."},
-  {title: "TA in Information Systems", time_period: "Sept & Oct 2023", institution: "CentraleSupélec", description: "Leading 20h of Information Systems and Programming tutorials for 30 students."},
-  {title: "Operator in Perfume Production", time_period: "July 2023", institution: "Dior", description: "Perfume manufacturing agent at Parfums Christian Dior's worldwide production and research center"},
+  {title: "TA in Computer Science", time_period: "Sept & Oct 2023", institution: "CentraleSupélec", description: "Leading 20h of Information Systems and Programming tutorials for 30 students."},
 ]
 
 
